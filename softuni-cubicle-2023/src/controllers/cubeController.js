678f7f5ee378932d644e8a5e4674b401f8fab8dd
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-01-27 19:52:32 +0200

    add accessoryShema and model

--- softuni-cubicle-2023/src/controllers/cubeController.js
+++ softuni-cubicle-2023/src/controllers/cubeController.js
@@ -1,19 +1,17 @@
 
 const Cube = require('../models/cube');
-// const db = require('../modelswefwefwe');
-
 
 exports.createView = (req, res) => {
-        res.render('create');
+    res.render('create');
 }
 
 exports.addCube = async (req, res) => {
     let cube = new Cube(req.body);
-    await cube.save()
+    await cube.save();
     res.redirect('/');
 }
 
 exports.detailsView = async (req, res) => {
     const selectedCube = await Cube.findById(req.params._id).lean();
-    res.render('details', {cube: selectedCube});
+    res.render('details', { cube: selectedCube });
 }
---
+++ softuni-cubicle-2023/src/models/accessory.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+
+const accessorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+     // validation for http/https
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLenght: 50,
+    }
+    
+});
+
+const Accessory = ('Accessory', accessorySchema);
+
+module.exports = Accessory;
