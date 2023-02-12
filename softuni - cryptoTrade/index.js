a823b6fed43f87e173b43a82d3f90cd605aed3bc
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-02-12 16:14:02 +0200

    run database and fix urls in navBar

--- softuni - cryptoTrade/index.js
+++ softuni - cryptoTrade/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = require('./router');
 const handlebars = require('express-handlebars');
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
 
 const app = express();
 
@@ -9,13 +12,24 @@ app.engine('hbs', handlebars.engine({
 }))
 
 app.set('view engine', 'hbs');
-  
+
+// if app.use( express.static('public')), every request get through public folder
+// if app.use( '/static', express.static('public')); only request who starts with ./static get through public folder; 
 app.use( '/static', express.static('public'));
+// app.use(express.urlencoded({extended: false})) -  parse url in object and can get him with req.body
 app.use(express.urlencoded({extended: false}));
 
 app.use(router);
 
 
 
-app.listen(5000, () => console.log("Server is listening on port 5000....") );
+mongoose.connect('mongodb://127.0.0.1:27017/crypto')
+.then(() => app.listen(5000, () => console.log("Database is loaded and Server is listening on port 5000....") ))
+.catch((error) => console.log(error)) 
+    
+
+
+
+
+// app.listen(5000, () => console.log("Server is listening on port 5000....") );
 
